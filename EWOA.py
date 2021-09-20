467dd2183112bc38469b043f7a644eb691fd3bc5
ZongSingHuang <a26833765@gmail.com>
2021-09-20 19:25:16 +0800

    1

--- EWOA.py
+++ EWOA.py
@@ -12,14 +12,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class EWOA():
-    def __init__(self, fit_func, num_dim=30, num_particle=20, max_iter=500, 
-                 b=1, x_max=1, x_min=0, a_max=2, a_min=0, l_max=1, l_min=-1, a2_max=-1, a2_min=-2):
-        self.fit_func = fit_func
-        self.num_dim = num_dim
-        self.num_particle = num_particle
-        self.max_iter = max_iter
-        self.x_max = x_max
-        self.x_min = x_min
+    def __init__(self, fitness, D=30, P=20, G=500, ub=1, lb=0,
+                 b=1, a_max=2, a_min=0, a2_max=-1, a2_min=-2, l_max=1, l_min=-1):
+        self.fitness = fitness
+        self.D = D
+        self.P = P
+        self.G = G
+        self.ub = ub
+        self.lb = lb
         self.a_max = a_max
         self.a_min = a_min
         self.a2_max = a2_max
@@ -28,51 +28,52 @@ class EWOA():
         self.l_min = l_min
         self.b = b
         
-        self._iter = 1
-        self.gBest_X = None
-        self.gBest_score = np.inf
-        self.gBest_curve = np.zeros(self.max_iter)
-        self.X = np.random.uniform(low=self.x_min, high=self.x_max, size=[self.num_particle, self.num_dim])
-        
-        score = self.fit_func(self.X)
-        self.gBest_score = score.min().copy()
-        self.gBest_X = self.X[score.argmin()].copy()
-        self.gBest_curve[0] = self.gBest_score.copy()
+        self.gbest_X = np.zeros([self.D])
+        self.gbest_F = np.inf
+        self.loss_curve = np.zeros(self.G)
         
     def opt(self):
+        # 初始化
+        self.X = np.random.uniform(low=self.lb, high=self.ub, size=[self.P, self.D])
         
-        
-        while(self._iter<self.max_iter):
-            a2 = self.a2_max - (self.a2_max-self.a2_min)*(self._iter/self.max_iter)
+        # 迭代
+        for g in range(self.G):
+            # 適應值計算
+            F = self.fitness(self.X)
             
-            for i in range(self.num_particle):
+            # 更新最佳解
+            if np.min(F) < self.gbest_F:
+                idx = F.argmin()
+                self.gbest_X = self.X[idx].copy()
+                self.gbest_F = F.min()
+            
+            # 收斂曲線
+            self.loss_curve[g] = self.gbest_F
+            
+            # 更新
+            a2 = self.a2_max - (self.a2_max-self.a2_min)*(g/self.G)
+            
+            for i in range(self.P):
                 p = np.random.uniform()
                 R2 = np.random.uniform()
                 C = 2*R2
                 l = (a2-1)*np.random.uniform() + 1
                 self.b = np.random.randint(low=0, high=500)
                 
-                if p>=0.5: # (5)
-                    D = np.abs(self.gBest_X - self.X[i, :])
-                    self.X[i, :] = D*np.exp(self.b*l)*np.cos(2*np.pi*l)+self.gBest_X
-                else: # (4)
-                    D = C*self.gBest_X - self.X[i, :]
-                    self.X[i, :] = self.gBest_X  - D*np.cos(2*np.pi*l) 
+                if p<0.5:
+                    Dnew = C*self.gbest_X - self.X[i, :] # (4)
+                    self.X[i, :] = self.gbest_X  - Dnew*np.cos(2*np.pi*l) # (4)
+                else:
+                    D = np.abs(self.gbest_X - self.X[i, :]) # (5)
+                    self.X[i, :] = self.gbest_X + D*np.exp(self.b*l)*np.cos(2*np.pi*l) # (5)
             
-            self.X = np.clip(self.X, self.x_min, self.x_max)
+            # 邊界處理
+            self.X = np.clip(self.X, self.lb, self.ub)
 
-            score = self.fit_func(self.X)
-            if np.min(score) < self.gBest_score:
-                self.gBest_X = self.X[score.argmin()].copy()
-                self.gBest_score = score.min().copy()
-                
-            self.gBest_curve[self._iter] = self.gBest_score.copy()
-            self._iter = self._iter + 1
-        
     def plot_curve(self):
         plt.figure()
-        plt.title('loss curve ['+str(round(self.gBest_curve[-1], 3))+']')
-        plt.plot(self.gBest_curve, label='loss')
+        plt.title('loss curve ['+str(round(self.loss_curve[-1], 3))+']')
+        plt.plot(self.loss_curve, label='loss')
         plt.grid()
         plt.legend()
         plt.show()
